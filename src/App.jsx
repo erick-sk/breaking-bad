2c3756276143b5b3c15ad492899c24f7ade597df
Erick Saavedra <esvdrah@gmail.com>
2022-04-02 12:10:37 -0500

    Add styles and logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import Logo from './breaking-bad-logo.svg';
+import styled from '@emotion/styled';
+
+const LogoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const BreakingBadLogo = styled.img`
+  max-width: 100%;
+`;
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <LogoContainer>
+        <BreakingBadLogo src={Logo} alt="breaking bad logo" />
+      </LogoContainer>
+    </>
+  );
 }
 
 export default App;
